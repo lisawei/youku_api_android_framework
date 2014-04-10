405da99ededd8c9320aee385228b562bcd33c4f8
edisonlz <edisonlz1@163.com>
2014-04-10 15:20:46 +0800

    delete unused

--- src/com/android/intro/moiveslist/models/PlayModel.java
+++ src/com/android/intro/moiveslist/models/PlayModel.java
@@ -17,7 +17,6 @@ import com.loopj.android.http.RequestParams;
 
 public class PlayModel extends BaseModel {
 
-	String Path = "v3/play/address";
 
 	public PlayModel(Context context) {
 		super(context);
@@ -25,60 +24,6 @@ public class PlayModel extends BaseModel {
 
 	public void get_playurl(String vid, final HttpCallBackHandler callbackhandler) {
 
-		String MoivesUrl = String.format("http://%s/%s", Host, Path);
-		Log.d("URL", MoivesUrl);
-
-		RequestParams params = new RequestParams();
-		params.put("id", vid);
-		params.put("pid", PID);
-		params.put("guid", GUID);
-		params.put("format", "6");
-
-		Header[] headers = {
-				new BasicHeader("Content-type", "application/json"),
-				new BasicHeader("Accept", "text/html,text/xml,application/xml"),
-				new BasicHeader("User-Agent", UserAgent) };
-
-		client.get(this.context, MoivesUrl, headers, params,
-				new JsonHttpResponseHandler() {
-
-					@Override
-					public void onSuccess(JSONObject document) {
-						JSONObject results;
-						JSONObject playinfo = null;
-						ArrayList<String> urls;
-						ArrayList<HashMap<String, String>> datas = new ArrayList<HashMap<String, String>>();
-
-						try {
-							results = document.getJSONObject("results");
-						} catch (JSONException e1) {
-							// TODO Auto-generated catch block
-							e1.printStackTrace();
-							return;
-						}
-						String key=null;
-
-						if (results.has("m3u8")) {
-							key = "m3u8";
-						}else if (results.has("m3u8_mp4")) {
-							key = "m3u8_mp4";
-						} 
-						
-						JSONArray data;
-						try {
-							data = results.getJSONArray(key);
-							playinfo = (JSONObject) data.get(0);
-						} catch (JSONException e) {
-							// TODO Auto-generated catch block
-							e.printStackTrace();
-						}
-
-						HashMap<String, Object> resultHash = new HashMap<String, Object>();
-						resultHash.put("playinfo", playinfo);
-						callbackhandler.onSuccess(resultHash);
-
-					}
-				});
 	}
 
 }
